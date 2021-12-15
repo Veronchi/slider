22934af26e0da82c278677cfc55d219341c14b08
Veronchi <zhigunova1995@gmail.com>
2021-12-15 14:55:13 +0300

    add layout to the app component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,33 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>
+          reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        <article className='activeSlide'>
+          <img src='' alt='' className='person-img' />
+          <h4>111</h4>
+          <p>2222</p>
+          <p>333</p>
+          <span className='icon'>
+            <FaQuoteRight />
+          </span>
+        </article>
+        <button className='prev'>
+          <FiChevronLeft />
+        </button>
+        <button className='next'>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
